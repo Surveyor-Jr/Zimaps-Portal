60877fe7b786a8d43a300cb103a703f3d60fc1ca
Surveyor Jr <admin@africansurveyors.net>
2021-02-17 10:11:33 +0200

    update

--- zimaps/settings.py
+++ zimaps/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media Management 
 MEDIA_URL = '/media/'
